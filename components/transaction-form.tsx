4f4a94dffb52be3d1b1981c40ee54aed80d8cf0d
Kylan Hurt <kylan.hurt@gmail.com>
2025-03-10 15:10:22 -0700

    WIP tx form issues with datepicker

--- components/transaction-form.tsx
+++ components/transaction-form.tsx
@@ -1,5 +1,174 @@
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { FormControl, FormField, FormItem, Form, FormLabel, FormMessage } from "./ui/form"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select"
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover"
+import { Calendar } from "./ui/calendar"
+import { Button } from "./ui/button"
+import { CalendarIcon } from "lucide-react"
+import { cn } from "@/lib/utils"
+import { format } from "date-fns"
+import { Input } from "./ui/input"
+
+
+const transactionFormSchema = z.object({
+  transactionType: z.enum(['income', 'expense']),
+  categoryId: z.coerce.number().positive("Please select a category"),
+  transactionDate: z.date().max(new Date(), "Transaction date cannot be in the future"),
+  amount: z.coerce.number().positive("Amount must be greater than 0"),
+  transactionDescription: z.string().min(3, "Description must contain at least 3 characters").max(300, "Description must be less than 300 characters"),
+})
+
 export const TransactionForm = () => {
+  const form = useForm<z.infer<typeof transactionFormSchema>>({
+    resolver: zodResolver(transactionFormSchema),
+    defaultValues: {
+      transactionType: 'income',
+      amount: 0,
+      categoryId: 0,
+      transactionDescription: '',
+      transactionDate: new Date(),
+    }
+  })
   return (
-    <div>Transaction Form</div>
+    <Form {...form}>
+      <form>
+        <fieldset disabled={form.formState.isSubmitting} className="grid grid-cols-2 gap-y-5 gap-x-2">
+          <FormField
+            control={form.control} // intellisense and type safety
+            name="transactionType"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>
+                    Transaction Type
+                  </FormLabel>
+                  <FormControl>
+                    <Select value={field.value} onValueChange={field.onChange}>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Transaction type" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="income">Income</SelectItem>
+                        <SelectItem value="expense">Expense</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )
+            }}
+          />
+
+          <FormField
+            control={form.control} // intellisense and type safety
+            name="categoryId"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>
+                    Category
+                  </FormLabel>
+                  <FormControl>
+                    <Select value={field.value.toString()} onValueChange={field.onChange}>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Category" />
+                      </SelectTrigger>
+                      <SelectContent>
+
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )
+            }}
+          />
+
+          <FormField
+            control={form.control} // intellisense and type safety
+            name="transactionDate"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>
+                    Transaction Date
+                  </FormLabel>
+                  <FormControl>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <div>
+                          <Button
+                            variant={"outline"}
+                            className={cn(
+                              "w-full justify-start text-left font-normal",
+                              !field.value && "text-muted-foreground"
+                            )}
+                          >
+                            <CalendarIcon className="mr-2 h-4 w-4" />
+                            {field.value ? format(field.value, "PPP") : <span>Pick a date</span>}
+                          </Button>
+                        </div>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0">
+                        <Calendar
+                          mode="single"
+                          selected={field.value}
+                          onSelect={field.onChange}
+                          initialFocus
+                          disabled={{ after: new Date()}}
+                        />
+                      </PopoverContent>
+                    </Popover>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )
+            }}
+          />  
+
+          <FormField
+            control={form.control} // intellisense and type safety
+            name="amount"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>
+                    Category
+                  </FormLabel>
+                  <FormControl>
+                    <Input {...field} type="number" step={0.01} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )
+            }}
+          />          
+
+        </fieldset>
+
+        <fieldset disabled={form.formState.isSubmitting}>
+          <FormField
+            control={form.control} // intellisense and type safety
+            name="transactionDescription"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>
+                    Description
+                  </FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )
+            }}
+          />
+          <Button type="submit">{form.formState.isSubmitting ? 'Submitting...' : 'Submit'}</Button>
+        </fieldset>
+      </form>
+    </Form>
   )
 } 
