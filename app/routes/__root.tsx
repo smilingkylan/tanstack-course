e533f3386137decf46fda76c9f2db3a95ee3bf93
Kylan Hurt <kylan.hurt@gmail.com>
2025-03-10 00:16:39 -0700

    Add Poppins font

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -8,6 +8,15 @@ import {
 } from '@tanstack/react-router'
 // ?url means not bundled but loaded remotely
 import appCss from '@/app/styles/globals.css?url'
+import poppins100 from '@fontsource/poppins/100.css?url';
+import poppins200 from '@fontsource/poppins/200.css?url';
+import poppins300 from '@fontsource/poppins/300.css?url';
+import poppins400 from '@fontsource/poppins/400.css?url';
+import poppins500 from '@fontsource/poppins/500.css?url';
+import poppins600 from '@fontsource/poppins/600.css?url';
+import poppins700 from '@fontsource/poppins/700.css?url';
+import poppins800 from '@fontsource/poppins/800.css?url';
+import poppins900 from '@fontsource/poppins/900.css?url';
 
 export const Route = createRootRoute({
   head: () => ({
@@ -26,6 +35,33 @@ export const Route = createRootRoute({
     links: [{
       rel: 'stylesheet',
       href: appCss,
+    }, {
+      rel: 'stylesheet',
+      href: poppins100,
+    }, {
+      rel: 'stylesheet',
+      href: poppins200,
+    }, {
+      rel: 'stylesheet',
+      href: poppins300,
+    }, {
+      rel: 'stylesheet',
+      href: poppins400,
+    }, {
+      rel: 'stylesheet',
+      href: poppins500,
+    }, {
+      rel: 'stylesheet',
+      href: poppins600,
+    }, {
+      rel: 'stylesheet',
+      href: poppins700,
+    }, {
+      rel: 'stylesheet',
+      href: poppins800,
+    }, {
+      rel: 'stylesheet',
+      href: poppins900,
     }]
   }),
   component: RootComponent,
